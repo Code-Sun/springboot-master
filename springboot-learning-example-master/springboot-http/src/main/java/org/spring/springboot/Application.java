df640d2888145779a858e2a410210e3e99a8989d
Administrator <756113663@qq.com>
2020-06-05 20:44:50 +0800

    springboot-http

---
+++ springboot-learning-example-master/springboot-http/src/main/java/org/spring/springboot/Application.java
@@ -0,0 +1,18 @@
+package org.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ * <p>
+ * Created by bysocket on 16/4/26.
+ */
+// Spring Boot 应用的标识
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
